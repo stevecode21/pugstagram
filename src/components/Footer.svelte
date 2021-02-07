6fd9da74210c90510dfb91b7f0fa9205c8dcdee2
stevecode <suarezsteve21@gmail.com>
2021-02-07 12:58:20 -0500

    App styles

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,3 +5,12 @@
     </div>
   </div>
 </div>
+
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -7,3 +7,9 @@
     <Card />
   </div>
 </div>
+
+<style>
+  .Timeline {
+    padding: 4em 0 0 0;
+  }
+</style>
