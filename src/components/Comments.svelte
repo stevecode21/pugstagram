de2fb4af49c18909e86cbc260eec70ab2ad3bcea
stevecode <suarezsteve21@gmail.com>
2021-02-07 13:52:34 -0500

    Comments component

--- src/components/Comments.svelte
+++ src/components/Comments.svelte
@@ -1,11 +1,31 @@
+<script>
+  export let comments = [];
+
+  const addComment = (event) => {
+    const msg = event.target.text.value;
+    if (msg.length > 3) {
+      const message = {
+        id: Date.now(),
+        text: msg,
+        username: "stevecode",
+      };
+      // concat
+      comments = [...comments, message];
+      event.target.text.value = "";
+    }
+  };
+</script>
+
 <div class="Comments">
   <div class="Comments-content">
-    <div class="Comments-users">
-      <h3>Crispeta</h3>
-      <span>Hello Elmo!</span>
-    </div>
+    {#each comments as comment (comment.id)}
+      <div class="Comments-users">
+        <h3>{comment.username}</h3>
+        <span>{comment.text}</span>
+      </div>
+    {/each}
     <div class="Comments-add">
-      <form>
+      <form on:submit|preventDefault={addComment}>
         <input
           type="text"
           class="Comments-input"
