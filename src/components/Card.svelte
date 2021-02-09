62b9d6876a578702589262c4ace07e18fa19c0b4
stevecode <suarezsteve21@gmail.com>
2021-02-08 21:21:23 -0500

    Modal component

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,11 +1,27 @@
 <script>
   import Comments from "./Comments.svelte";
-
+  import Modal from "./Modal.svelte";
+  import Share from "./Share.svelte";
+  import { blur } from "svelte/transition";
   // Import the post data
   export let username, location, photo, postComment, comments, avatar;
+
+  let isModal = false;
+
+  const handleClick = () => {
+    isModal = !isModal;
+  };
 </script>
 
 <div class="Card">
+  {#if isModal}
+    <div transition:blur>
+      <Modal>
+        <Share on:click={handleClick} />
+      </Modal>
+    </div>
+  {/if}
+
   <div class="Card-container">
     <div class="Card-header">
       <div class="Card-user">
@@ -27,7 +43,7 @@
     <div class="Card-icons">
       <div class="Card-icons-first">
         <i class="fas fa-heart" />
-        <i class="fas fa-paper-plane" />
+        <i class="fas fa-paper-plane" on:click={handleClick} />
       </div>
       <div class="Card-icons-second">
         <i class="fas fa-bookmark" />
